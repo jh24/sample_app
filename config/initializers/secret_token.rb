a3e5fbc06f6a88495555b86b9d75d07f41e4948b
Jason Heaton <jh24@live.missouristate.edu>
2014-02-26 14:50:04 -0600

    Remove Hello World page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '2972925bd60a4d69f9a71468b2fbcad86c609a27bf435be8bbbc850d7b18ef4c2e0a9a3549a88c0c8aa928d75526af7c1f525eea518f4a99105681f0e23f5dfa'
